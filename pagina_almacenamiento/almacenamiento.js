5edbbb19c1893026eae7f2dbaea394d17b793ace
Artik-1210 <zazopro34@gmail.com>
2025-05-16 08:49:51 +0000

    16/05/25

--- pagina_almacenamiento/almacenamiento.js
+++ pagina_almacenamiento/almacenamiento.js
@@ -202,7 +202,7 @@ function showDeleteConfirmModal(filePath) {
             <p>¿Estás seguro de que quieres eliminar este archivo o carpeta? Todo su contenido será eliminado.</p>
           </div>
           <div class="modal-footer">
-            <button type="button" id="cancelDeleteBtn" class="btn btn-danger">Cancelar</button>
+            <button type="button" id="cancelDeleteBtn" class="btn btn-danger" background-color="grey">Cancelar</button>
             <button type="button" id="confirmDeleteBtn" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
